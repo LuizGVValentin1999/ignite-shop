d71319a33e0413654dad9e9639dd5630b4852b7d
LuizGVValentin1999 <59962559+LuizGVValentin1999@users.noreply.github.com>
2023-04-07 11:02:08 -0300

    libk to home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,15 +4,19 @@ import logoImg from "../assets/logo.svg"
 import { Container, Header } from "../styles/pages/app"
 
 import Image from "next/image"
+import Link from "next/link"
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
-      <Header>
-        <Image src={logoImg} alt="" />
-      </Header>
+      <Link href='/'>
+        <Header>
+          <Image src={logoImg} alt="" />
+        </Header>
+      </Link>
+    
 
       <Component {...pageProps} />
     </Container>
